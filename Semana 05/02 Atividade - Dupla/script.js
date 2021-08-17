ff9dc1d0b2f7555c1ca71c8914f61094427b51aa
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2021-08-17 11:11:14 -0300

    Fix folder name

---
+++ Semana 05/02 Atividade - Dupla/script.js
@@ -0,0 +1,68 @@
+// 1
+// $("h2").css("background-color", "green");
+ 
+// 2
+// $(".buttons").parent().css("background-color", "red");
+ 
+// 3
+// $(".module > * ").css("background-color", "blue");
+// $(".module").children().css("background-color", "blue");
+ 
+// 4
+// $(".module ul li").eq(2).css("background-color", "green");
+ 
+// 5
+// $("#myList li").first().css("background-color", "yellow");
+ 
+// 6
+// $("#slideshow li").last().css("background-color", "black");
+ 
+// 7
+// if ($("#search").children().first().is("form")) {
+//     alert("O elemento form é filho de search");
+// }
+// else {
+//     alert("O elemento form não é filho de search");
+// }
+ 
+// 8
+// $("ul").not("#slideshow").css("background-color", "blue");
+ 
+// 9
+// $("li").has("p").css("background-color", "pink");
+ 
+// 10
+// $("#myListItem").text("Este é um novo item da lista");
+ 
+// 11
+// $(".selected").next().css("background-color", "grey");
+ 
+// 12
+// $(".input_text").prev().css("background-color", "grey");
+ 
+// 13
+// $("#listItem_2").siblings().css("background-color", "green");
+ 
+// 14
+// $("[src]").css("background-color", "yellow");
+ 
+// 15
+// $("#header #nav .selected");
+ 
+// 16
+// $("[type='text']");
+ 
+// 17
+// $("[value]");
+ 
+// 18
+// $("[type!='submit']").css("background-color", "red");
+ 
+// 19
+// $("[href^='blog']").css("background-color", "green");
+ 
+// 20
+// $("[value$='a']").css("background-color", "red");
+ 
+// 21
+// $("[href*='html']").css("background-color", "grey");
